2151cb560148bec69575fc62dc6840d5972d1d61
sadhna vishnukant birajdar <sadhnabirajdar898@gmail.com>
2026-07-22 15:42:51 +0530

    url fix

--- apps/api/src/config/db.ts
+++ apps/api/src/config/db.ts
@@ -3,11 +3,12 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASSWORD,
+    // port: Number(process.env.DB_PORT),
+    connectionString: process.env.PG_LIVE_URL
 })
 
 export default pool
